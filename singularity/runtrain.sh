1fe00c10a08ca2594ddf09e14111580eed67696e
William Ashbee <wsashbee@gmail.com>
2023-03-20 14:55:21 +0000

    updating from benchmarking

--- singularity/runtrain.sh
+++ singularity/runtrain.sh
@@ -6,8 +6,8 @@
 #SBATCH --gres=gpu:V100:1
 #SBATCH -t 5-00:00
 #SBATCH -J v2ctrain
-#SBATCH -e /data/users2/washbee/Vox2Cortex_fork/jobs/error%A.err
-#SBATCH -o /data/users2/washbee/Vox2Cortex_fork/jobs/out%A.out
+#SBATCH -e jobs/error%A.err
+#SBATCH -o jobs/out%A.out
 #SBATCH -A psy53c17
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=washbee1@student.gsu.edu
@@ -18,6 +18,6 @@ sleep 5s
 source /usr/share/lmod/lmod/init/bash
 module use /application/ubuntumodules/localmodules
 module load singularity/3.10.2
-singularity exec --nv --bind /data/users2/washbee/speedrun/Vox2Cortex_fork/:/v2c,/data/users2/washbee/hcp-plis-subj-v2c:/hcp-plis-subj /data/users2/washbee/containers/speedrun/v2c_sr.sif /v2c/singularity/train.sh &
+singularity exec --nv --bind /data/users2/washbee/speedrun/Vox2Cortex_fork/:/v2c,/data/users2/washbee/speedrun/v2c-data:/hcp-plis-subj /data/users2/washbee/containers/speedrun/v2c_sr.sif /v2c/singularity/train.sh &
 wait
 
